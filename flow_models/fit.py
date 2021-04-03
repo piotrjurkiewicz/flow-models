Fix integer overflow during int casting
On 64-bit Unix systems astype(int) is equivalent to astype(np.int64), however on Windows and 32-bit Unix casting is performed to np.int32. Use np.int64 explicitly to avoid this.

Fixes #1.

flow_models/fit.py
@@ -219,7 +219,7 @@ def gui(**kwargs):
 
     dd = load_data([kwargs['path']])
     df = list(dd.values())[0]
-    idx = np.unique(np.rint(np.geomspace(df.index.min(), df.index.max(), LINE_NBINS)).astype(int))
+    idx = np.unique(np.rint(np.geomspace(df.index.min(), df.index.max(), LINE_NBINS)).astype(np.int64))
 
     first_plot = False
 

flow_models/lib/plot.py
@@ -107,7 +107,7 @@ def plot_pdf(data, idx=None, x_val='length', what='flows', mode=frozenset(['poin
 
     if isinstance(data, pd.DataFrame):
         if idx is None:
-            idx = np.unique(np.rint(np.geomspace(data.index.min(), data.index.max(), LINE_NBINS)).astype(int))
+            idx = np.unique(np.rint(np.geomspace(data.index.min(), data.index.max(), LINE_NBINS)).astype(np.int64))
 
         data = data.loc[:, 'bin_hi':what + '_sum']
 
@@ -168,7 +168,7 @@ def plot_cdf(data, idx=None, x_val='length', what='flows', mode=frozenset(['mixt
     plots = []
     if isinstance(data, pd.DataFrame):
         if idx is None:
-            idx = np.unique(np.rint(np.geomspace(data.index.min(), data.index.max(), LINE_NBINS)).astype(int))
+            idx = np.unique(np.rint(np.geomspace(data.index.min(), data.index.max(), LINE_NBINS)).astype(np.int64))
         if 'line' in mode:
             cdfd = data[what + '_sum'].cumsum() / data[what + '_sum'].sum()
             cdfi = scipy.interpolate.interp1d(cdfd.index, cdfd, 'linear', bounds_error=False)(idx)
@@ -185,7 +185,7 @@ def plot_avg(data, idx=None, x_val='length', what='packets', mode=frozenset(['mi
     plots = []
     if isinstance(data, pd.DataFrame):
         if idx is None:
-            idx = np.unique(np.rint(np.geomspace(data.index.min(), data.index.max(), LINE_NBINS)).astype(int))
+            idx = np.unique(np.rint(np.geomspace(data.index.min(), data.index.max(), LINE_NBINS)).astype(np.int64))
 
         if what in ['packets', 'octets']:
             avg_points, avg_line = avg_data(data, idx, what)

flow_models/plot.py
@@ -36,7 +36,7 @@ def plot(objects, x_val='length', ext='png', single=False, normalize=True, fft=F
 
     for obj, df in data.items():
         if idx is None:
-            idx = np.unique(np.rint(np.geomspace(df.index.min(), df.index.max(), LINE_NBINS)).astype(int))
+            idx = np.unique(np.rint(np.geomspace(df.index.min(), df.index.max(), LINE_NBINS)).astype(np.int64))
         for what in ['flows', 'packets', 'octets']:
             logmsg('Drawing CDF', obj, what)
             plot_cdf(df, idx, x_val, what, mode={'line', 'mixture', *cdf_modes})