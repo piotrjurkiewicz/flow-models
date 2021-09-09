722c97828e43481b12235013a40473f11b098f79
Piotr Jurkiewicz <piotr.jerzy.jurkiewicz@gmail.com>
2021-09-09 14:18:43 +0200

    Add filtering capabilities to convert tool

--- flow_models/convert.py
+++ flow_models/convert.py
@@ -8,7 +8,7 @@ import argparse
 from .lib.io import io_parser, IN_FORMATS, OUT_FORMATS
 from .lib.util import logmsg, prepare_file_list
 
-def convert(in_files, out_file, in_format='nfcapd', out_format='csv_flow'):
+def convert(in_files, out_file, in_format='nfcapd', out_format='csv_flow', count=0, skip_input=0, skip_output=0, filter_expr=None):
     """
     Convert one flow format to another.
 
@@ -22,6 +22,14 @@ def convert(in_files, out_file, in_format='nfcapd', out_format='csv_flow'):
         input format (Default is 'nfcapd')
     out_format : str, optional
         output format (Default is 'csv_flow')
+    count : int, optional
+        number of flows to write (Default is 0 (all flows))
+    skip_input : int, optional
+        number of flows to skip at the beginning of input (Default is 0)
+    skip_output : int, optional
+        number of flows to skip after filtering (Default is 0)
+    filter_expr : str, optional
+        filter expression (Default is None)
     """
 
     reader, writer = IN_FORMATS[in_format], OUT_FORMATS[out_format]
@@ -33,8 +41,17 @@ def convert(in_files, out_file, in_format='nfcapd', out_format='csv_flow'):
 
     for file in in_files:
         for flow in reader(file):
-            writer.send(flow)
-            written += 1
+            if skip_input > 0:
+                skip_input -= 1
+            else:
+                if filter_expr is None or eval(filter_expr):
+                    if skip_output > 0:
+                        skip_output -= 1
+                    else:
+                        writer.send(flow)
+                        written += 1
+                        if written == count:
+                            break
         logmsg(f'Finished: {file}. Written: {written}')
 
     writer.close()
@@ -43,6 +60,10 @@ def convert(in_files, out_file, in_format='nfcapd', out_format='csv_flow'):
 
 def parser():
     p = argparse.ArgumentParser(description=__doc__, parents=[io_parser])
+    p.add_argument('--count', type=int, default=0, help='number of flows to copy')
+    p.add_argument('--skip-input', type=int, default=0, help='number of flows to skip at the beginning of input')
+    p.add_argument('--skip-output', type=int, default=0, help='number of flows to skip after filtering')
+    p.add_argument('--filter', default=None, help='expression of filter')
     return p
 
 def main():
@@ -53,7 +74,12 @@ def main():
     else:
         input_files = prepare_file_list(app_args.files)
 
-    convert(input_files, app_args.O, app_args.i, app_args.o)
+    if app_args.filter:
+        filter_expr = compile(app_args.filter, '<filter>', 'eval')
+    else:
+        filter_expr = None
+
+    convert(input_files, app_args.O, app_args.i, app_args.o, app_args.count, app_args.skip_input, app_args.skip_output, filter_expr)
 
 
 if __name__ == '__main__':
