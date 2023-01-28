c518fc109b57f308563a361fb6b3c7c3b91a5c2d
Piotr Jurkiewicz <piotr.jerzy.jurkiewicz@gmail.com>
2023-01-28 01:07:00 +0100

    Speed up series tool

--- flow_models/series.py
+++ flow_models/series.py
@@ -4,8 +4,6 @@ Generates packets and octets time series from flow records.
 """
 import collections
 
-import numpy as np
-
 from .lib.io import IOArgumentParser, IN_FORMATS
 from .lib.util import logmsg
 
@@ -42,35 +40,42 @@ def series(in_files, in_format='nfcapd', count=None, skip_input=0, skip_output=0
     counters = {'count': count, 'skip_input': skip_input, 'skip_output': skip_output}
     written = 0
 
-    day_packets = collections.defaultdict(lambda: np.zeros(86400, 'Q'))
-    day_octets = collections.defaultdict(lambda: np.zeros(86400, 'd'))
+    dd = collections.defaultdict(lambda: [[0, 0] for _ in range(86400)])
 
     for file in in_files:
         for key, first, first_ms, last, last_ms, packets, octets, aggs in reader(file, counters=counters, filter_expr=filter_expr, key_fields=key_fields, val_fields=val_fields):
             day = first // 86400
             second_in_day = first % 86400
+            d = dd[day]
             if packets == 1:
-                day_packets[day][second_in_day] += 1
-                day_octets[day][second_in_day] += octets
+                d[second_in_day][0] += 1
+                d[second_in_day][1] += octets
             else:
-                ms_in_day = float(second_in_day * 1000 + first_ms)
-                duration = (last - first) * 1000 + last_ms - first_ms
+                float_second_in_day = second_in_day + first_ms / 1000
+                duration = last - first + (last_ms - first_ms) / 1000
                 piat = duration / packets
                 avg_packet_size = octets / packets
+                int_packet_size = avg_packet_size.__trunc__()
                 for n in range(packets):
-                    second_in_day = int(ms_in_day // 1000)
-                    day_packets[day][second_in_day] += 1
-                    day_octets[day][second_in_day] += avg_packet_size
-                    ms_in_day += piat
-                    while ms_in_day >= 86400000:
+                    packet_size = int_packet_size + (octets - int_packet_size - avg_packet_size * (packets - 1 - n)).__trunc__()
+                    ds = d[float_second_in_day.__trunc__()]
+                    ds[0] += 1
+                    ds[1] += packet_size
+                    octets -= packet_size
+                    float_second_in_day += piat
+                    while float_second_in_day >= 86400.0:
                         day += 1
-                        ms_in_day -= 86400000
+                        float_second_in_day -= 86400.0
+                        d = dd[day]
+                assert octets == 0
             written += 1
         logmsg(f'Finished: {file}. Written: {written}')
 
-    for day in day_packets:
-        day_packets[day].tofile(f"{day}.packets", '\n', "%d")
-        day_octets[day].tofile(f"{day}.octets", '\n', "%d")
+    for day in dd:
+        with open(f"{day}.packets", 'w') as p, open(f"{day}.octets", 'w') as o:
+            for second_in_day in range(86400):
+                p.write("%d\n" % dd[day][second_in_day][0])
+                o.write("%d\n" % dd[day][second_in_day][1])
 
     logmsg(f'Finished all files. Written: {written}')
 
