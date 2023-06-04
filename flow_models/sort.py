39b9b7af0b87163d629ea6e69740093bd17914d4
Piotr Jurkiewicz <piotr.jerzy.jurkiewicz@gmail.com>
2023-06-04 23:59:49 +0200

    Make sort use load_arrays

--- flow_models/sort.py
+++ flow_models/sort.py
@@ -3,32 +3,32 @@
 Sorts flow records according to specified fields (requires `numpy`).
 """
 
-import argparse
 import pathlib
 
 import numpy as np
 
-from .lib.io import load_array_np
-from .lib.util import logmsg, measure_memory, prepare_file_list
+from .lib.io import load_array_np, prepare_file_list, load_arrays, IOArgumentParser
+from .lib.util import logmsg, measure_memory
 
-def create_index(key_files, index_file, reverse=False):
+def create_index(path, key_fields, index_file, counters=None, reverse=False):
+
+    if counters is None:
+        counters = {'skip_in': 0, 'count_in': None, 'skip_out': 0, 'count_out': None}
+
     keys = []
-    size = None
 
     logmsg('Create index: loading key files')
 
-    if not key_files:
-        logmsg('At least one key file must be specified when creating an index.')
+    if not key_fields:
+        logmsg('At least one key field must be specified when creating an index.')
         raise ValueError
 
-    for path in key_files:
-        name, dtype, mm = load_array_np(path)
-        if size is None:
-            size = mm.size
-        else:
-            assert mm.size == size
-        keys.append(mm)
-        del mm
+    arrays, filtered, size = load_arrays(path, key_fields, counters, None)
+
+    for name in key_fields:
+        keys.append(arrays[name])
+        del arrays[name]
+    del arrays
 
     logmsg('Create index: sorting keys')
 
@@ -74,7 +74,11 @@ def create_index(key_files, index_file, reverse=False):
     else:
         return result
 
-def sort_array(input_file, output_dir, index_array):
+def sort_array(input_file, output_dir, index_array, counters=None):
+
+    if counters is None:
+        counters = {'skip_in': 0, 'count_in': None, 'skip_out': 0, 'count_out': None}
+
     logmsg('Sort array: loading:', input_file)
 
     size = index_array.size
@@ -88,6 +92,10 @@ def sort_array(input_file, output_dir, index_array):
         mode = 'r'
 
     name, dtype, in_mm = load_array_np(input_file, mode)
+    if counters['skip_in'] > 0:
+        in_mm = in_mm[counters['skip_in']:]
+    if counters['count_in'] is not None and counters['count_in'] > 0:
+        in_mm = in_mm[:counters['count_in']]
     assert in_mm.size == size
 
     if mode == 'r':
@@ -120,15 +128,44 @@ def sort_array(input_file, output_dir, index_array):
 
     del out_mm
 
+def sort(in_files, out_dir, in_format='binary', out_format='binary', key_fields=(), index_file=None, skip_in=0, count_in=None, skip_out=0, count_out=None, filter_expr=None, no_index=False, reverse=False):
+
+    assert in_format == 'binary'
+    assert out_format == 'binary'
+    if count_out is not None:
+        raise NotImplementedError
+    if skip_out != 0:
+        raise NotImplementedError
+    if filter_expr is not None:
+        raise NotImplementedError
+
+    counters = {'skip_in': skip_in, 'count_in': count_in, 'skip_out': skip_out, 'count_out': count_out}
+
+    if no_index:
+        index = create_index(in_files, key_fields, None, counters, reverse)
+    else:
+        try:
+            _, _, index = load_array_np(index_file)
+            logmsg('Using existing index file...')
+        except FileNotFoundError:
+            logmsg('Index file not exists, will be created...')
+            create_index(in_files, key_fields, index_file, counters, reverse)
+            _, _, index = load_array_np(index_file)
+
+    for f in prepare_file_list(in_files):
+        sort_array(f, out_dir, index, counters)
+
 def parser():
-    p = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, description=__doc__)
-    p.add_argument('-I', default='__index', help='index file suffix')
-    p.add_argument('-k', nargs='*', help='ordered key array files')
-    p.add_argument('-O', help='directory for output')
+    p = IOArgumentParser(description=__doc__)
+    p._option_string_actions['-i'].choices = ['binary']
+    p._option_string_actions['-i'].default = 'binary'
+    p._option_string_actions['-o'].choices = ['binary']
+    p._option_string_actions['-o'].default = 'binary'
+    p.add_argument('-k', '--key-files', nargs='*', help='ordered key fields names')
+    p.add_argument('-I', '--index-file', default='_index', help='index file')
     p.add_argument('--no-index', action='store_true', help='do not save index into file')
     p.add_argument('--reverse', action='store_true', help='reverse order')
     p.add_argument('--measure-memory', action='store_true', help='collect and print memory statistics')
-    p.add_argument('files', nargs='+', help='array files to sort')
     return p
 
 def main():
@@ -139,19 +176,8 @@ def main():
         raise ValueError
 
     with measure_memory(app_args.measure_memory):
-        if app_args.no_index:
-            index = create_index(app_args.k, None, app_args.reverse)
-        else:
-            try:
-                _, _, index = load_array_np(app_args.I)
-                logmsg('Using existing index file...')
-            except FileNotFoundError:
-                logmsg('Index file not exists, will be created...')
-                create_index(app_args.k, app_args.I, app_args.reverse)
-                _, _, index = load_array_np(app_args.I)
-
-        for f in prepare_file_list(app_args.files):
-            sort_array(f, app_args.O, index)
+        delattr(app_args, 'measure_memory')
+        sort(**vars(app_args))
 
 
 if __name__ == '__main__':
