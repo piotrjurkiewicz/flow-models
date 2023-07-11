8ab75edaa3417aaa0a5315478d61176be1513534
Piotr Jurkiewicz <piotr.jerzy.jurkiewicz@gmail.com>
2023-07-12 00:27:35 +0200

    Update series_plot tool

--- flow_models/series_plot.py
+++ flow_models/series_plot.py
@@ -1,26 +1,34 @@
-import pathlib
 import datetime
+import pathlib
 
-import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
+import pandas as pd
 
-for f in sorted(pathlib.Path(".").glob("*.packets")):
-    try:
-        title = datetime.datetime.utcfromtimestamp(int(f.stem) * 86400).strftime('%Y-%m-%d-%a')
-    except ValueError:
-        title = f.stem
-    s = pd.read_csv(f, header=None)
-    ax = s.plot(kind='area', linewidth=0, alpha=0.5, legend=None, ylabel='packets/s', xlabel='second', xlim=(0, 86400), title=title)
-    # s.rolling(600, min_periods=1).mean().plot(kind='line', linewidth=0.2, color='k', legend=None, ax=ax)
-    plt.show()
+from flow_models.lib.plot import PDF_NONE
 
-for f in sorted(pathlib.Path(".").glob("*.octets")):
-    try:
-        title = datetime.datetime.utcfromtimestamp(int(f.stem) * 86400).strftime('%Y-%m-%d-%a')
-    except ValueError:
-        title = f.stem
-    s = pd.read_csv(f, header=None)
-    s *= 8
-    ax = s.plot(kind='area', linewidth=0, alpha=0.5, legend=None, ylabel='bits/s', xlabel='second', xlim=(0, 86400), title=title)
-    # s.rolling(600, min_periods=1).mean().plot(kind='line', linewidth=0.2, color='k', legend=None, ax=ax)
-    plt.show()
+matplotlib.rcParams['pdf.use14corefonts'] = True
+matplotlib.rcParams['font.family'] = 'sans'
+
+for what in ['flows', 'packets', 'octets']:
+    for f in sorted(pathlib.Path('.').glob(f'*.{what}')):
+        s = pd.read_csv(f, header=None)
+        if what == 'octets':
+            s *= 8
+            what = 'bits'
+        try:
+            title = f"{datetime.datetime.utcfromtimestamp(int(f.stem) * 86400).strftime('%Y-%m-%d-%a')} (Day {f.stem})"
+        except ValueError:
+            title = f"Day {f.stem}"
+        print(title, what, sum(s[0]))
+        title = None
+        ax = s.rolling(30, min_periods=1).mean().plot(kind='line', linewidth=0.2, color='k', legend=None, ylabel=f'{what.capitalize()[:-1]} rate [{what}/s]', xlabel='Time [s]', xlim=(0, 86400), title=title)
+        plt.ylim(bottom=0)
+
+        # plt.axvspan(19 * 3600, 20 * 3600, linewidth=0,  color='lightsteelblue')
+        # s = pd.read_csv(f.absolute().parent.parent / 'dorms' / f.name, header=None)
+        # if what == 'bits':
+        #     s *= 8
+        # s.rolling(30, min_periods=1).mean().plot(ax=ax, kind='line', linewidth=0.2, color='r', legend=None)
+
+        plt.savefig(f'{f.stem}.{what}.pdf', bbox_inches='tight', metadata=PDF_NONE)
